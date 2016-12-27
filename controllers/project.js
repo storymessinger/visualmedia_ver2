aa90fd2ca714cf1ab6590da3c81c1181957bae54
Jun <Jun@Junui-MacBook-Pro.local>
2016-12-27 15:18:03 +0900

    disabled db

--- controllers/project.js
+++ controllers/project.js
@@ -4,18 +4,19 @@ var Project = require('../models/project_db');
 
 router.get('/:id', function(req, res) {
 
-    Project.get(req.params.id, function(err, data){
-
-        if(err){
-        }
-        // checking it in the console
-        console.log(data);
-
-        res.render('project',{
-            //안에 들어갈거 : 여기서 정의된거
-            data:data
-        });
-    });
+    // Project.get(req.params.id, function(err, data){
+    //
+    //     if(err){
+    //     }
+    //     // checking it in the console
+    //     console.log(data);
+    //
+    //     res.render('project',{
+    //         //안에 들어갈거 : 여기서 정의된거
+    //         data:data
+    //     });
+    // });
+    res.render('project');
 });
 
 module.exports = router;
